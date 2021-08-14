8442c07a3646792741051437710f7329738f1c35
kirknaoborot <drishik05@gmail.com>
2021-08-14 09:10:14 +0300

    Добавьте файлы проекта.

---
+++ RecognizingVin/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RecognizingVin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите путь к файлу изображения с именем файла и расширением: ");
+
+            string filePath = Console.ReadLine();
+
+            var result = new IronOcr.IronTesseract().Read(filePath);
+
+            var text = result.Text.Replace("O", "0");
+
+            Console.ReadKey();
+
+            Console.WriteLine(text);
+        }
+    }
+}
